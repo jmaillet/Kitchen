e16347f7db1625392a927b0dce0c042be6f62ecb
jmaillet <maillet.john@gmail.com>
2016-02-06 22:01:13 -0500

    Update logger.ts
    checks log level

--- src/Kitchen/src/common/logger.ts
+++ src/Kitchen/src/common/logger.ts
@@ -1,38 +1,57 @@
 import * as LogManager from 'aurelia-logging';
 import 'toastr';
+import settings from '../settings';
+
+
+export enum LogLevel {
+    Error,
+    Warn,
+    Info,
+    Debug
+    
+}
 
 export class Logger {
 
     logger: LogManager.Logger;
     constructor(private loggerName: string) {
         this.logger = LogManager.getLogger(loggerName);
+        this.logLevel = settings.logLevel || LogLevel.Error;
     }
 
     debug(message: string, showToast?: boolean) {
-        this.logger.debug(message);
-        if (showToast) {
-            toastr.info(message);
+        if(this.logLevel <= LogLevel.Debug) {
+            this.logger.debug(message);
+            if (showToast) {
+                toastr.info(message);
+            }
         }
     }
 
     info(message: string, showToast?: boolean) {
-        this.logger.info(message);
-        if (showToast) {
-            toastr.info(message);
+        if(this.logLevel <= LogLevel.Info) {
+            this.logger.info(message);
+            if (showToast) {
+                toastr.info(message);
+            }
         }
     }
 
     warn(message: string, showToast?: boolean) {
-        this.logger.warn(message);
-        if (showToast) {
-            toastr.warning(message);
+        if(this.logLevel <= LogLevel.Warn) {
+            this.logger.warn(message);
+            if (showToast) {
+                toastr.warning(message);
+            }
         }
     }
 
     error(message: string, showToast?: boolean) {
-        this.logger.error(message);
-        if (showToast) {
-            toastr.error(message);
+        if(this.logLevel <= LogLevel.Error) {
+            this.logger.error(message);
+            if (showToast) {
+                toastr.error(message);
+            }
         }
     }
 }
