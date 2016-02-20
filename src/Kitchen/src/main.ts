61b3d776323d1f4ca3d78db12ec66678840d23b7
jmaillet <maillet.john@gmail.com>
2016-02-19 22:11:31 -0500

    branch for new work

--- src/Kitchen/src/main.ts
+++ src/Kitchen/src/main.ts
@@ -7,11 +7,6 @@ export function configure(aurelia: Aurelia) {
         .developmentLogging()
         .plugin('aurelia-breeze');
 
-  //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
-
-  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  //aurelia.use.plugin('aurelia-html-import-template-loader')
 
   aurelia.start().then(a => a.setRoot());
 }
--- src/Kitchen/src/recipes/index.ts
+++ src/Kitchen/src/recipes/index.ts
@@ -1,21 +1,34 @@
 
 import {Router, RouterConfiguration} from 'aurelia-router'
 import {Logger} from '../common/logger';
+import {RecipeService} from 'services/recipe-dataservice';
+import {Recipe} from 'models/recipe'
 
 export class RecipesSection {
     
     logger: Logger;
     router: Router;
-    constructor() {
+    recipes: Recipe[]
+    constructor(private recipeService: RecipeService) {
         this.logger = new Logger('App');
+    }
+
+    activate(): Promise<any> {
+        return this.recipeService.get()
+            .then(result => { this.recipes = result; })
+            .catch(reason => { this.logger.error(reason, true); });
+    }
+
+    edit(id: number) {
+        this.router.navigate('recipes/' + id);
     }
 
     configureRouter(config: RouterConfiguration, router: Router) {
         this.logger.debug("configuring Router ...");
         config.title = 'Kitchen';
         config.map([
-            { route: '',  moduleId: './recipe-list'},
-            { route: ':id', moduleId: './recipe' }
+            { route: '', moduleId: 'none', title: 'Select' },
+            { route: ':id', moduleId: './recipe',  }
 
         ]);
 
